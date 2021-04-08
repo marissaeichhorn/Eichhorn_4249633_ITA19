5a4fa889af9ad9a0bfd7669d483e03f277f2352a
marissaeichhorn <64789851+marissaeichhorn@users.noreply.github.com>
2021-04-08 15:29:40 +0200

    Add files via upload
    Backup

---
+++ LinearRegressionDataFormatter.m
@@ -0,0 +1,57 @@
+classdef LinearRegressionDataFormatter < matlab.mixin.SetGet
+    %DATAPRERPERATOR Class to model the training data for lineare
+    %regression model object
+   
+    properties (Access = public)
+        feature
+        featureName
+        commandVar
+        commandVarName
+        numOfSamples
+    end
+    
+    properties (Access = private)
+        data
+    end
+    
+    methods (Access = public)
+        function obj = LinearRegressionDataFormatter(varargin)
+            
+            %DATAPRERPERATOR Construct an instance of this class
+            
+            % ========= YOUR CODE HERE ========= 
+            % perform the varargin daten einlesen
+            
+            for i = 1:length(varargin)
+                if strcmp(varargin{i}, 'Data')
+                    obj.data = importdata(varargin{i+1});
+                elseif strcmp(varargin{i}, 'Feature')
+                    obj.featureName = varargin{i+1};
+                elseif strcmp(varargin{i}, 'CommandVar')
+                    obj.commandVarName = varargin{i+1};
+                end
+            end
+            
+            % ========= YOUR CODE END ========= 
+            
+            obj.mapFeature();
+            obj.mapCommandVar();
+            obj.mapNumOfSamples();
+        end
+    end
+    
+    methods (Access = private)
+        function mapFeature(obj)
+            obj.feature = obj.data.(obj.featureName);
+        end
+        
+        function mapCommandVar(obj)
+            obj.commandVar = obj.data.(obj.commandVarName);
+        end
+        
+        function mapNumOfSamples(obj)
+           obj.numOfSamples = length(obj.feature); 
+        end
+    end
+end
+
